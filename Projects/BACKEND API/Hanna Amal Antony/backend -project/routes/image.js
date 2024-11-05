6ebbeb4b4758a635cd11ab4210b60e7da2556f52
hannahamal <hannamanalur@gmail.com>
2024-11-05 15:08:17 +0530

    Hanna-Backend_Chnage2

--- Projects/BACKEND API/Hanna Amal Antony/backend -project/routes/image.js
+++ Projects/BACKEND API/Hanna Amal Antony/backend -project/routes/image.js
@@ -4,18 +4,38 @@ const path = require('path');
 var router = express.Router();
 
 
+
+
 const app = express();
-const upload=multer({
-  dest:"uploads/",
+
+
+// Configure Multer storage options
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    // Use the original filename or customize it
+    cb(null, Date.now() + path.extname(file.originalname)); // Appending the timestamp for uniqueness
+  }
 });
 
-router.post("/uploads",upload.single("file"),(req,res)=>{
+const upload = multer({ storage });
 
- res.json({
+// Define the upload route
+router.post('/uploads', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'No file uploaded'
+    });
+  }
+  if(req.file)
+  res.json({
     status: 'success',
     message: 'File uploaded successfully',
-    
+    file: req.file // Include file details in the response if needed
   });
+});
 
-})
 module.exports=router;
